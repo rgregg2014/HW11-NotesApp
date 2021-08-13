e01812d62d51fb659d7403f920c6892d38ac5b4b
Rachel Gregg <rgregg2014@gmail.com>
2021-08-13 15:13:23 -0400

    basic server functionality to deploy on Heroku

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 //need middleware stuff from starwars demos to make anything do anything
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
